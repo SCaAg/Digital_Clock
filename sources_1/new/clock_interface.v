78b2ff98e101b490798a034122964a86c62cd94e
lilliu <mu777666555@gmail.com>
2024-09-03 03:02:56 +0000

    chore: Update clock_interface module to include new features and fix bugs

--- sources_1/new/clock_interface.v
+++ sources_1/new/clock_interface.v
@@ -20,11 +20,12 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 module clock_interface (
-    input  wire clk,
-    input  wire rst_n,
-    input  wire mode_btn,
-    input  wire adjust_btn,
-    input  wire down_btn,
+    input wire clk,
+    input wire rst_n,
+    input wire mode_btn,
+    input wire adjust_btn,
+    input wire down_btn,
+    input wire [63:0] counter,
     output wire test
 );
 
@@ -93,12 +94,116 @@ module clock_interface (
     end
   end
 
+
+
+
+
+  wire [15:0] bcd_editing;
+  reg  [15:0] bcd_editing_tmp;
+  reg  [15:0] bcd_editing_max;
+  bcd_increment_16bit bcd_increment (
+      .bcd_in (bcd_editing_tmp),
+      .bcd_max(bcd_editing_max),
+      .bcd_out(bcd_editing),
+  );
+
   // pressed down_btn
+  always @(posedge clk or negedge rst_n) begin
+    if (~rst_n) begin
+      bcd_editing_tmp <= 16'b0;
+    end else if (~down_btn) begin
+      bcd_editing_tmp <= bcd_editing;
+    end
+  end
+
+
+
+  reg  [ 5:0] led0;  //5:blink,4:dot,[3:0]:bcd
+  reg  [ 5:0] led1;
+  reg  [ 5:0] led2;
+  reg  [ 5:0] led3;
+  reg  [ 5:0] led4;
+  reg  [ 5:0] led5;
+  reg  [ 5:0] led6;
+  reg  [ 5:0] led7;
+
+  reg  [ 5:0] led0_tmp;  //5:blink,4:dot,[3:0]:bcd
+  reg  [ 5:0] led1_tmp;
+  reg  [ 5:0] led2_tmp;
+  reg  [ 5:0] led3_tmp;
+  reg  [ 5:0] led4_tmp;
+  reg  [ 5:0] led5_tmp;
+  reg  [ 5:0] led6_tmp;
+  reg  [ 5:0] led7_tmp;
+
+  wire [15:0] year_bcd;
+  wire [ 7:0] month_bcd;
+  wire [ 7:0] day_bcd;
+  wire [ 7:0] hour_bcd;
+  wire [ 7:0] minute_bcd;
+  wire [ 7:0] second_bcd;
+  wire [13:0] year;
+  wire [ 3:0] month;
+  wire [ 4:0] day;
+  wire [ 2:0] weekday;
+  wire [ 4:0] hour;
+  wire [ 5:0] minute;
+  wire [ 5:0] second;
+
+  stamp2time stamp2time0 (
+      .clk(clk),
+      .rst_n(rst_n),
+      .counter(counter),
+      .year_bcd(year_bcd),
+      .month_bcd(month_bcd),
+      .day_bcd(day_bcd),
+      .hour_bcd(hour_bcd),
+      .minute_bcd(minute_bcd),
+      .second_bcd(second_bcd),
+      .year(year),
+      .month(month),
+      .day(day),
+      .weekday(weekday),
+      .hour(hour),
+      .minute(minute),
+      .second(second)
+  );
+  // led drive block
   always @(posedge clk or negedge rst_n) begin
     if (~rst_n) begin
 
     end else begin
+      // in the display mode
+      if (edit_state == EDIT_IDLE) begin
+        case (disp_state)
+          TIME_DISP: begin
+            led7 <= {2'b01, hour_bcd[7:4]};
+            led6 <= {2'b01, hour_bcd[3:0]};
+            led5 <= {2'b01, 4'd10};
+            led4 <= {2'b01, minute_bcd[7:4]};
+            led3 <= {2'b01, minute_bcd[3:0]};
+            led2 <= {2'b01, 4'd10};
+            led1 <= {2'b01, second_bcd[7:4]};
+            led0 <= {2'b01, second_bcd[3:0]};
+          end
+          DATE_DISP: begin
+            led7 <= {2'b01, year_bcd[15:12]};
+            led6 <= {2'b01, year_bcd[11:8]};
+            led5 <= {2'b01, year_bcd[7:4]};
+            led4 <= {2'b00, year_bcd[3:0]};
+            led3 <= {2'b01, month_bcd[7:4]};
+            led2 <= {2'b00, month_bcd[3:0]};
+            led1 <= {2'b01, second_bcd[7:4]};
+            led0 <= {2'b00, second_bcd[3:0]};
+          end
+          ALARM_DISP: begin
 
+          end
+          TIMER_DISP: edit_state <= EDIT_TIMER;
+          STOP_WATCH_DISP: edit_state <= EDIT_IDLE;
+          default: ;
+        endcase
+      end
     end
   end
 endmodule
